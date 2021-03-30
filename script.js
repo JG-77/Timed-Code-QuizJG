905839a51665af612e05095f9d5ff298c1a6a79d
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-03-29 21:36:53 -0700

    Timer countdown function created.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 var timer = document.getElementById("timer");
 var startbtn = document.getElementById("startbtn");
 
-var time = 60 
+var time = 60;
+
+startbtn.addEventListener("click", function(){
+    var timeInterval = setInterval(function(){
+        time--;
+        timer.textContent = "Time: " + time;
+        if (time === 0)
+            clearInterval(timeInterval);
+    }, 1000)
+})
