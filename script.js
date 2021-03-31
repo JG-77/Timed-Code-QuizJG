f6ddda01027105ab344afaa66e688b0db16e85ba
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-03-30 18:14:13 -0700

    separated function and event listener for timer.

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ var startbtn = document.getElementById("startbtn");
 
 var time = 60;
 
-startbtn.addEventListener("click", function(){
-    var timeInterval = setInterval(function(){
+//Clicking start button begins timer
+startbtn.addEventListener("click", startTime);
+
+//function for timer
+function startTime() {
+    var timeInterval = setInterval(function() {
         time--;
         timer.textContent = "Time: " + time;
         if (time === 0)
             clearInterval(timeInterval);
     }, 1000)
-})
+}
