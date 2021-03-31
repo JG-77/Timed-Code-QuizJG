723457d033a6709e29179dca539787e39d092012
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-03-30 18:48:32 -0700

    set up win functions and extended timer function.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 var timer = document.getElementById("timer");
 var startbtn = document.getElementById("startbtn");
+var bttn = document.querySelectorAll("button");
 
 var time = 60;
-var winValid = false;
+var winner = false;
 
 //Clicking start button begins quiz
 startbtn.addEventListener("click", startQuiz);
 
 //function for starting quiz
 function startQuiz() {
-    winValid = false;
+    winner = false;
     time = 60;
     startTime()
 }
@@ -19,15 +20,32 @@ function gameWin() {
 
 }
 
+//function checking win
+function winValid() {
+    //finish if statement
+    if () {
+        winner = true;
+    }
+}
+
 //function for timer
 function startTime() {
     var timeInterval = setInterval(function() {
         time--;
         timer.textContent = "Time: " + time;
-        if (time === 0)
-            clearInterval(timeInterval);
-            //define function
-            loseGame();
-``
+        if(time >= 0) {
+            //if time runs out player loses
+            if (time === 0) {
+                clearInterval(timeInterval);
+                //define function
+                gameLoss();
+                }
+            //if player completes quiz before time runs out, they pass    
+            if (winner && time > 0) {
+                clearInterval(timeInterval);  
+                gameWin();
+            }
+
+        }
     }, 1000)
 }
